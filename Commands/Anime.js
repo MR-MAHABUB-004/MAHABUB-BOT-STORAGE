fbb39f2cd579bc5f2fe098ba420de5623561ae13
MR᭄﹅ MAHABUB﹅ メꪜ <mrmahabub345@gmail.com>
2025-02-27 14:16:09 +0600

    Create Anime.js

---
+++ Commands/Anime.js
@@ -0,0 +1,14 @@
+{
+  "videos": [
+    "https://i.imgur.com/lLTTRgx.mp4",
+"https://i.imgur.com/mNGFGWd.mp4",
+"https://i.imgur.com/Av0I6kd.mp4",
+"https://i.imgur.com/IT68gkr.mp4",
+"https://i.imgur.com/QNsVlAz.mp4",
+"https://i.imgur.com/mdyhVSR.mp4"
+"https://i.imgur.com/rO88Rtb.mp4",
+"https://i.imgur.com/D2rSL80.mp4",
+"https://i.imgur.com/JjI5x0o.mp4",
+"https://i.imgur.com/siRLWDm.mp4"
+  ]
+}
